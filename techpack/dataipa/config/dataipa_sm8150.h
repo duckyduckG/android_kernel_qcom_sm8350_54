afde96028e063142765c0f5f69a617a6d34d2e77
duckyduckG <duckyduckg65@gmail.com>
2025-07-30 14:45:12 +0500

    [ DNM ]: techpack: dataipa: enable and disable sm8150 specific configs

--- techpack/dataipa/config/dataipa_sm8150.h
+++ techpack/dataipa/config/dataipa_sm8150.h
@@ -4,8 +4,8 @@
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
-#define CONFIG_GSI 1
-#define CONFIG_GSI_REGISTER_VERSION_2 1
+#define CONFIG_GSI 0
+#define CONFIG_GSI_REGISTER_VERSION_2 0
 #define CONFIG_RMNET_IPA3 1
 #define CONFIG_RNDIS_IPA 1
 #define CONFIG_IPA_WDI_UNIFIED_API 1
@@ -14,3 +14,5 @@
 #define CONFIG_IPA3_REGDUMP_IPA_4_5 1
 #define CONFIG_IPA3_4_5_RGSTR 0
 #define CONFIG_IPA3_REGDUMP_NUM_EXTRA_ENDP_REGS 0
+#define CONFIG_IPA3_MHI_PROXY 1
+#define CONFIG_IPA3_MHI_PRIME_MANAGER 1
